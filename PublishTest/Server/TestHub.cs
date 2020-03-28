6bf7724f3ea31b297c80a3979dd5eb7062fdd26d
Dragan Pepic <d.pepic@hotmail.com>
2020-03-28 19:15:25 +0100

    Add project files.

---
+++ PublishTest/Server/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublishTest.Server
+{
+	public class TestHub : Hub
+	{
+		public async Task TestMetoda()
+		{
+			await Clients.Caller.SendAsync("KlijentTest");
+		}
+	}
+}
